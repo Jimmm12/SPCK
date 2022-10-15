0f4149060ca641ce06e0906f7b1807db7cf0d20c
Huy Trần <trantienhuy1112@gmail.com>
2022-10-15 14:47:52 +0700

    cho api vao  gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import Header from "./Nav/Header";
 import Footer from "./Nav/Footer";
-import Destination from "./component/Destination";
-import Gallery from "./component/Gallery";
-import Blogs from "./component/Blogs";
-import Home from "./component/Home";
-import { Routes, Route } from 'react-router-dom';
+import Destination from "./Destination/Destination";
+import Gallery from "./Gallery/Gallery";
+import Blogs from "./Blogs/Blogs";
+import Home from "./Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header/> 
-        <Routes>
-       
-          <Route path="/" element={<Home />} exact="true" />
-        
-          <Route path="/Destination" element={<Destination />} />
-          <Route path="/Gallery" element={<Gallery />} />
-          <Route path="/Blogs" element={<Blogs />} /> 
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} exact="true" />
+        <Route path="/Destination" element={<Destination />} />
+        <Route path="/Gallery" element={<Gallery />} />
+        <Route path="/Blogs" element={<Blogs />} />
+      </Routes>
       <Footer />
     </div>
   );
